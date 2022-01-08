2b684679d0f2acb65788f54cc1d6a39f868620bc
deepsingh <deepsingh.cts@gmail.com>
2022-01-08 23:36:14 +0530

    registration-validations and styles

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,19 +8,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
-  public registerForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    passphrase: new FormControl(''),
-  })
+  constructor(private fb: FormBuilder) { }
+
+  firstName = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  lastName = new FormControl('');
+  email = new FormControl('', [Validators.required]);
+  passphrase = new FormControl('', [Validators.required, Validators.minLength(6)]);
+
+  public registerForm = this.fb.group({
+    firstName: this.firstName,
+    lastName: this.lastName,
+    email: this.email,
+    passphrase: this.passphrase
+  });
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
     console.log('===', this.registerForm.value)
+    this.registerForm.reset();
   }
 
 }
