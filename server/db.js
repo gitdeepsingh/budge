e98151cf871263e2cf33f1bb1555a2acff771779
deepsingh <deepsingh.cts@gmail.com>
2022-01-11 19:50:16 +0530

    db connection refactored

--- server/db.js
+++ server/db.js
@@ -1,24 +1,16 @@
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require('pg');
+const connectionString = 'postgresql://postgres:Bang@707@localhost:5432/budge';
+
 const pool = new Pool({
-  user: 'dbuser',
-  host: 'database.server.com',
-  database: 'mydb',
-  password: 'secretpassword',
-  port: 3211,
+    connectionString,
 })
 pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+    if (err) console.log('Error while querying pool. Reason=', err);
+    else { }
+    pool.end();
+});
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'Bang@707',
-  port: 5432,
-})
-client.connect()
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+    connectionString,
+});
+
+module.exports = client;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool, Client } = require('pg');
-
-const connectionString = 'postgresql://postgres:Bang@707@localhost:5432/postgres'
 
 const corsMiddleware = require('./middlewares/corsHelper');
+const db = require('./db');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -18,32 +16,29 @@ app.post('/registration', (req, res) => {
 
     res.json(req.body);
 });
-const pool = new Pool({
-    connectionString,
-})
-pool.query('SELECT NOW()', (err, res) => {
-    if (err) console.log('err=', err);
-    else console.log('res=', res);
-    pool.end();
-});
-const client = new Client({
-    connectionString,
-});
 
 
 // server start
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server is up at port ${port}`);
-    client.connect().then(() => {
+    try {
+        await db.connect();
         console.log('Successfully connected to DB');
-        client.query('SELECT NOW()', (err, res) => {
+        db.query('SELECT NOW()', (err, res) => {
             if (err) console.log('c err=', err);
             else console.log('c res=', res);
-            client.end()
+            db.end()
         })
-    }).catch(e => {
-        console.log('Error connecting to DB. Reason=', e, JSON.stringify(e));
-    })
+    } catch (e) {
+        console.log('Error connecting to the db. Reason=', e, JSON.stringify(e));
+        console.log(' Exiting now...')
+        process.exit();
+    }
 });
+
+process.on('uncaughtException', () => {
+    console.log('UncaughtException occurred! Exiting now...')
+    process.exit();
+})
 
 module.exports = { app }
