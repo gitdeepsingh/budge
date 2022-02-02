3f38cfe90aeb2be0833ede65e57d9c88c16ce12d
deepsingh <deepsingh.cts@gmail.com>
2022-02-03 01:53:38 +0530

    create expense-ui-wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,29 @@ import { HighchartsChartModule } from 'highcharts-angular';
 // components
 import {MaterialModule} from './app.material';
 
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DownloadsComponent } from './reports/downloads/downloads.component';
-import { TrendsComponent } from './reports/trends/trends.component';
 import { AppMenuComponent } from './app-menu/app-menu.component';
+import { HomeComponent } from './home/home.component';
+import { TrendsComponent } from './reports/trends/trends.component';
+import { DownloadsComponent } from './reports/downloads/downloads.component';
+import { CreateExpenseComponent } from './expenses/createExpense/createExpense.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
     LoginComponent,
+    AppMenuComponent,
     HomeComponent,
     DownloadsComponent,
     TrendsComponent,
-    AppMenuComponent,
+    CreateExpenseComponent
   ],
   imports: [
     BrowserModule,
--- src/app/expenses/createExpense/createExpense.component.ts
+++ src/app/expenses/createExpense/createExpense.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
+import { HttpService } from './../../config/config.service';
 
 @Component({
   selector: 'app-create-expense',
   templateUrl: './createExpense.component.html',
-  styleUrls: ['./createExpense.component.css']
+  styleUrls: ['./createExpense.component.css'],
+  providers: [HttpService]
 })
+@Injectable()
 export class CreateExpenseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private http: HttpService) { }
+
+  expenseType = new FormControl(null, [Validators.required]);
+  expenseAmount = new FormControl(null, [Validators.required, Validators.min(1), Validators.max(999999999)]);
+  expenseDesc = new FormControl(null, [Validators.maxLength(250)]);
+
+  public expenseCreateForm = this.fb.group({
+    expenseType: this.expenseType,
+    expenseAmount: this.expenseAmount,
+    expenseDesc: this.expenseDesc,
+  });
 
   ngOnInit(): void {
+  }
+
+  onSubmit():void {
+    console.log(' this.expenseCreateForm.value: ',  this.expenseCreateForm.value);
+    this.expenseCreateForm.reset();
+  }
+
+  onReset():void {
+    this.expenseCreateForm.reset();
   }
 
 }
